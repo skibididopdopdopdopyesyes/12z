99e9dda56cd7b3d9088a910be5d0c303dbdbfb28
skibididopdopdopdopyesyes <skibididopdopdopdopyesyes0@gmail.com>
2026-06-02 12:02:34 +0300

    Create SialikVisualsMod.java

---
+++ src/main/java/com/swill/sialikvisuals/SialikVisualsMod.java
@@ -0,0 +1,10 @@
+package com.swill.sialikvisuals;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SialikVisualsMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        // Пустышка
+    }
+}
